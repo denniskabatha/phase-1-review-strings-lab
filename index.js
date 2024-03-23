e010c272dc843e67659a21e0849ca2f322db7818
denniskabatha <162292636+denniskabatha@users.noreply.github.com>
2024-03-23 13:38:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+const firstInitial = currentUser[0];
+      const restOfName = currentUser.slice(1);
+
+
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`
+
+
+
+
